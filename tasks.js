04ec345fede55dfa9695b51bf87fac6912588a32
kishan-745 <groot9877@gmail.com>
2024-07-18 01:50:09 +0530

    Variables and data type

---
+++ tasks.js
@@ -0,0 +1,45 @@
+// Day 1 : Variables and Data type
+
+// Activity 1: Variable declaration
+
+var num = 7;
+console.log(num);
+
+let str= "kishan"
+console.log(str);
+
+// Activity 2: constant Declarattion 
+
+const bool = true
+console.log(bool);
+
+// Activity 3: Data type
+
+let obj = {
+    id : 1,
+    dept: "cs"
+}
+
+let arr = [2,3,4,5,6]
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Activity 4: Reassigning Variable
+
+let reAss = 64;
+console.log(reAss);
+
+reAss = "kashyap"
+console.log(reAss);
+
+// Activity 5: understanding const
+
+const reAss1 = "kishan"
+console.log(reAss1); 
+
+// reAss1 = "1";
+// console.log(reAss1);
